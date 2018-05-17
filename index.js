88e6a83e0bc8c23a454c6eacea2c90d7b1f600c6
AMKlein2018 <adamklein0519@gmail.com>
2018-05-17 10:00:07 -0400

    May 17th

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // *******************************
  //Word and Hint Function
 
+
+
+var myInterval;
 wordArray = []
 hintsArray = []
 
@@ -12,14 +15,14 @@ function Word (word,hint){
 
 var one = new Word ("code", "Decipher") 
 var two = new Word ("break", "___ a Leg")
-var three = new Word ("Door", "Knob" )
+var three = new Word ("door", "Knob" )
 var four = new Word ("friend", "Opposite of Foe")
 var five = new Word ("smile", "Teeth")
 var six = new Word ("create", "Opposite of Destroy") 
-var seven = new Word ("Fast", "Slow")
+var seven = new Word ("fast", "Slow")
 var eight = new Word ("chair", "Sit")
 
-
+//Used Letters Container
 var usedLetters = document.getElementsByClassName("usedLetters")
 
 
@@ -60,64 +63,62 @@ wrapper.style.backgroundColor = "lightblue"
 wrapper.style.padding = "10px"
 wrapper.style.height = "200px"
 wrapper.style.position = "relative"
-wrapper.style.top = "250px"
+wrapper.style.top = "150px"
 document.body.appendChild(wrapper)
 
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+var wrongArray = []
+  
+  for (i = 0; i < 26; i++){
+    var alpha = document.createElement('div')
+    alpha.style.width = '40px'
+    alpha.style.border = '2px solid green'
+    alpha.style.textAlign = 'center'
+    alpha.style.display = 'inline-block'
+    alpha.style.margin = '5px'
+    alpha.classList.add('a')
+    // alpha.className = 'a'
+    alpha.innerHTML = alphabet[i]
+    wrapper.appendChild(alpha)
 
-	for (i = 0; i < 26; i++){
-		var alpha = document.createElement('div')
-		alpha.style.width = '40px'
-		alpha.style.border = '2px solid green'
-		alpha.style.textAlign = 'center'
-		alpha.style.display = 'inline-block'
-		alpha.style.margin = '5px'
-		alpha.classList.add('a')
-		// alpha.className = 'a'
-		alpha.innerHTML = alphabet[i]
-		wrapper.appendChild(alpha)
-
-		var buttons = document.getElementsByClassName('a')
-		buttons[i].addEventListener('click', function(event){
+    var buttons = document.getElementsByClassName('a')
+    buttons[i].addEventListener('click', function(event){
+      console.log(event.target.innerHTML)
         for (var i = 0; i < currentWord.length; i++)
           if (event.target.innerHTML === currentWord[i]) {
               splitWord.splice(i, 1, currentWord[i])
               wordBox.innerHTML = splitWord.join(" ")
+              
 
-          } else {
-              // var wrongArray = []
-              // wrongArray.push(event.target)
-              // usedLetters.innerHTML=wrongArray
-          }
-        // console.log(event.target.innerHTML)
+          } 
+              usedLetters[0].innerHTML += event.target.innerHTML;
 
-		})
-    // console.log(splitWord)
-	}
+    })
+      
+  }
 
 
 // console.log(rand)
 
 //Word Generator - red box
 
-
 var wordGen = document.createElement('button')
-	wordGen.style.width = '20%'
-   	wordGen.style.border = '2px solid blue'
-   	wordGen.style.textAlign = 'center'
-   	wordGen.style.height = "50px"
-   	wordGen.style.backgroundColor = 'gold'
-   	wordGen.innerHTML = "Word Generator"
-   	wordGen.style.position = "relative"
-   	wordGen.style.left = "1100px"
-   	wordGen.style.bottom = "100px"
+  wordGen.style.width = '20%'
+    wordGen.style.border = '2px solid blue'
+    wordGen.style.textAlign = 'center'
+    wordGen.style.height = "50px"
+    wordGen.style.backgroundColor = 'gold'
+    wordGen.innerHTML = "Word Generator"
+    wordGen.style.position = "relative"
+    wordGen.style.left = "1100px"
+    wordGen.style.bottom = "200px"
     wordGen.style.fontSize = "20px"
-   	var page = document.getElementsByTagName('body')[0];
-   	page.appendChild(wordGen)
-	 // wordBox.push(randomWord[i] + randomHint[i])
-	
+    var page = document.getElementsByTagName('body')[0];
+    page.appendChild(wordGen)
+   // wordBox.push(randomWord[i] + randomHint[i])
+  
 
 //Hint Generator 
 
@@ -131,7 +132,7 @@ var hintGen = document.createElement('button')
     hintGen.innerHTML = "Need A Hint?"
     hintGen.style.position = "relative"
     hintGen.style.left = "765px"
-    hintGen.style.bottom = "0px"
+    hintGen.style.bottom = "100px"
     hintGen.style.fontSize = "20px"
     var page = document.getElementsByTagName('body')[0];
     page.appendChild(hintGen)
@@ -141,24 +142,34 @@ var hintGen = document.createElement('button')
 //WORDBOX - CONTAINER
 
 var wordBox = document.createElement('div')
-	wordBox.style.width = "30%"
-	wordBox.style.margin = '0 auto'
-	wordBox.style.border = "2px solid orange"
-	wordBox.style.backgroundColor = "lightblue"
-	wordBox.style.height = "200px"
-	wordBox.style.position = 'relative'
-	// wrapper3.style.left = "50px"
-	wordBox.style.bottom = "300px"
+  wordBox.style.width = "30%"
+  wordBox.style.margin = '0 auto'
+  wordBox.style.border = "2px solid orange"
+  wordBox.style.backgroundColor = "lightblue"
+  wordBox.style.height = "200px"
+  wordBox.style.position = 'relative'
+  // wrapper3.style.left = "50px"
+  wordBox.style.bottom = "400px"
   wordBox.style.display = "block"
   wordBox.style.fontSize = "0px"
   wordBox.style.textAlign = "center"
   wordBox.style.paddingTop = "50px"
-	// wrapper2.style.bottom = "200px"
-	document.body.appendChild(wordBox)
+  // wrapper2.style.bottom = "200px"
+  document.body.appendChild(wordBox)
+
+  //WORD GENERATOR BUTTON FUNCTIONALITY FOR WORDBOX//
 
   wordGen.addEventListener('click', function(){
+  randomNum = random()
+  // Create Split Word Function
+  splitWord = wordArray[randomNum].split('')
+   // console.log(splitWord)
+  currentWord = wordArray[randomNum].split('')
+
   wordBox.style.fontSize = "30px"
   wordBox.innerHTML = stringDash(splitWord)
+
+
 })
   hintGen.addEventListener('click',function(){
   wordBox.style.fontSize = "30px"
@@ -169,12 +180,23 @@ var wordBox = document.createElement('div')
 
 
 
-
 //Timer 
 // var existingIntervalId = 0;
+wordGen.addEventListener('click', function () {
+    clearInterval(myInterval)
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#time');
+
+    startTimer(twoMinutes, display);
+
+    //Start the Timer
+});
+
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    setInterval(function () {
+    
+        myInterval = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -185,22 +207,16 @@ function startTimer(duration, display) {
 
         if (--timer < 0) {
             timer = duration;
-        }
-    }, 1000);
+        } 
+    } ,1000);
 
+  }
 
-}
-
-  var existingIntervalId = 0;
-  wordGen.addEventListener('click', function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    //Start the Timer
-    startTimer(fiveMinutes, display);
+// setTimeout(() => alert("Game Over"), 0);
+//               alert("Game Over");
 
 
 
-});
 
 
 
@@ -228,13 +244,6 @@ function startTimer(duration, display) {
 // }
 
 
-// result = function () {
-// 	wordBox = document.getElementById('wordBox')
-// 	correct = document.createElement('ul');
 
-// 	for (var i = 0; i <word.length; i++) {
-// 		correct.setAttribute('')
-// 	}
-// }
 
 
